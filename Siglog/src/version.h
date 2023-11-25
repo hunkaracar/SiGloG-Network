f6315d15edd6de3d29679287b93daddcd4955027
hunkaracr1 <114934461+hunkaracar@users.noreply.github.com>
2023-11-25 17:03:31 +0300

    Update version.h

--- Siglog/src/version.h
+++ Siglog/src/version.h
@@ -4,7 +4,7 @@
 
 void show_version(){
 	
-	char version1[] = "\nSıGloG(C) 2.I Software Dev.Inc.\n\n";
+	char version1[] = "\nSıGloG(C) V.2.I Software Dev.Inc.\n\n";
 	char version2[] = "This software is a Security Software.It is the first version of the product.\nCopyright and developed by (C) SiGloG Software Dev.Inc.\n\n";
 	
 	printf("%s",version1);
